9805cdd1016015047ecbf011f3516007a4961d5e
scardinanoire <andre.corsi@outlook.it>
2022-11-23 11:13:35 +0100

    si mette tutto online

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("hello hello");
+console.log("tutto funziona alla perfezione");
+
+let express = require("express");
+
+let app = express();
+
+let port = 3000;
+
+let server = app.listen(port);
+
+app.use(express.static("public"));
+
+console.log("questa roba gira su http://localhost:" + port);
