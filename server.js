ab53bf6475d388e3c139351cbe8192d63415be0f
scardinanoire <andre.corsi@outlook.it>
2022-11-23 12:30:42 +0100

    websocket fatto

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 console.log("tutto funziona alla perfezione");
 
 let express = require("express");
+const { connected } = require("process");
 
 let app = express();
 
@@ -11,3 +12,13 @@ let server = app.listen(port);
 app.use(express.static("public"));
 
 console.log("questa roba gira su http://localhost:" + port);
+
+let serverSocket = require("socket.io");
+
+let io = serverSocket(server);
+
+io.on("connection", newConnection);
+
+function newConnection(newSocket) {
+  console.log(newSocket.id);
+}
